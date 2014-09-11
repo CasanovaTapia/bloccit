cac5ffd3cc203a51005729a306f37223f8d357fe
Ching Casanova Tapia <ching@casanovatapia.com>
2014-09-11 11:14:14 +0800

    Added email confirmation and edited registration view

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Made with my mentor at [Bloc](http://bloc.io).
 See my version of [Bloccit here](http://ct-bloccit.herokuapp.com/).
 
+### SETUP
+
+Configure config/application.yml upon setup. See config/application.example.yml for template.
+
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  # config.secret_key = '61af8d0341a55315d5ad19d736dff5cef7947a1bd92b38659877f0ebd3acda4dbd10e9af1b03c1d67b625abe124b1d0cb03cc695c9bc0a0e1264eafdf5944432'
+  config.secret_key = ENV['DEVISE_SECRET_KEY']
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
