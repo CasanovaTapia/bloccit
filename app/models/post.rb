fc8efbe9d179a3026ca048682743768c0887946e
Ching Casanova Tapia <ching@casanovatapia.com>
2014-09-18 19:19:08 +0800

    Voting on posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,18 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
   belongs_to :user
   belongs_to :topic
   mount_uploader :image, ImageUploader
 
-  default_scope { order('created_at DESC') }
+  default_scope { order('rank DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20}, presence: true
-  validates :topic, presence: true
-  validates :user, presence: true
+  # validates :topic, presence: true
+  # validates :user, presence: true
+
+  after_create :create_vote
 
   def markdown_title
     render_as_markdown self.title
@@ -19,6 +22,26 @@ class Post < ActiveRecord::Base
     render_as_markdown self.body
   end
 
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    votes.sum(:value)
+  end
+
+  def update_rank
+    age = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24)
+    new_rank = points + age
+
+    update_attribute(:rank, new_rank)
+  end
+
+
   private
 
   def render_as_markdown(text)
@@ -27,4 +50,9 @@ class Post < ActiveRecord::Base
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render text).html_safe
   end
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end
+
 end
