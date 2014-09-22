8e3a16107dbcc56b94b9edfa1e99c9a03ff09485
Ching Casanova Tapia <ching@casanovatapia.com>
2014-09-22 13:57:56 +0800

    Cleaned up vote views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,22 @@ module ApplicationHelper
       content_tag :div, capture(&block), class: 'form-group'
     end
   end
+
+  def up_vote_link_classes(post)
+    vote = current_user.voted(post)
+    if vote && vote.up_vote?
+      "glyphicon glyphicon-chevron-up voted"
+    else
+      "glyphicon glyphicon-chevron-up "
+    end
+  end
+
+  def down_vote_link_classes(post)
+    vote = current_user.voted(post)
+    if vote && vote.down_vote?
+      "glyphicon glyphicon-chevron-down voted"
+    else
+      "glyphicon glyphicon-chevron-down "
+    end
+  end
 end
